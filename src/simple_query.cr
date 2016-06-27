9361c7466c842671a6a07069f902eb3665b6b138
Bri in The Sky <brianh6854@googlemail.com>
2016-06-27 13:32:12 +0100

    Updated to remove mid

--- src/simple_query.cr
+++ src/simple_query.cr
@@ -7,11 +7,11 @@ class QueryError < Exception; end
 class InternalError < Exception; end
 class TimeoutError < Exception; end
 
-host = "172.17.0.2"
+host = "127.0.0.1"
 port = 50000
 username = "monetdb"
 password = "monetdb"
-db = "threatmonitor"
+db = "test"
 
 mero = MonetDB::ClientJSON.new
 mero.host = host
@@ -20,11 +20,11 @@ mero.username = username
 mero.password = password
 mero.db = db
 
-myschema = "\"threatmonitor\""
+myschema = "\"test\""
 
 query = "SELECT * FROM #{myschema}.fruits;"
 mid = mero.connect
-result_json = mero.query_json(mid, query)
+result_json = mero.query_json(query)
 result = mero.json_to_hash(result_json)
 puts "Hash Created:".colorize(:red)
 p result
