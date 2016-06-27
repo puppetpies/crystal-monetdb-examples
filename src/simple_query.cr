f293c5223de412ae093623caeccc16ceedc97d6c
Bri in The Sky <brianh6854@googlemail.com>
2016-06-27 15:46:35 +0100

    Fixed require and added a few more queries

--- src/simple_query.cr
+++ src/simple_query.cr
@@ -1,5 +1,6 @@
-require "../libs/crystal-monetdb-libmapi/monetdb"
-require "../libs/crystal-monetdb-libmapi/monetdb_data"
+require "crystal-monetdb-libmapi/monetdb"
+require "crystal-monetdb-libmapi/monetdb_data"
+require "crystal-monetdb-libmapi/tools"
 require "colorize"
 
 class ConnectionError < Exception; end
@@ -21,7 +22,6 @@ mero.password = password
 mero.db = db
 
 myschema = "\"test\""
-
 query = "SELECT * FROM #{myschema}.fruits;"
 mero.connect
 result_json = mero.query_json(query)
@@ -32,3 +32,42 @@ puts "Display specific fields".colorize(:red)
 result.each {|k,v|
   puts "Hash Key: #{k} Name: #{v["name"]} Price: #{v["price"]}"
 }
+
+puts "Peach"
+query = "SELECT * FROM #{myschema}.fruits WHERE name = 'Peach';"
+result_json = mero.query_json(query)
+result = mero.json_to_hash(result_json)
+puts "Hash Created:".colorize(:red)
+p result
+puts "Display specific fields".colorize(:red)
+result.each {|k,v|
+  puts "Hash Key: #{k} Name: #{v["name"]} Price: #{v["price"]}"
+}
+
+puts "Collection of fruits of 9.99"
+query = "SELECT * FROM #{myschema}.fruits WHERE price = '9.99';"
+result_json = mero.query_json(query)
+result = mero.json_to_hash(result_json)
+puts "Hash Created:".colorize(:red)
+p result
+puts "Display specific fields".colorize(:red)
+result.each {|k,v|
+  puts "Hash Key: #{k} Name: #{v["name"]} Price: #{v["price"]}"
+}
+
+dur = Timers.new
+dur.start
+puts "GUID test"
+query = "SELECT * FROM #{myschema}.guid_test;"
+result_json = mero.query_json(query)
+result = mero.json_to_hash(result_json)
+puts "Hash Created:".colorize(:red)
+p result
+puts "Display specific fields".colorize(:red)
+result.each {|k,v|
+  puts "Hash Key: #{k} F1: #{v["f1"]} F2: #{v["f2"]}"
+}
+puts "Total time:"
+dur.stop
+puts dur.stats
+
